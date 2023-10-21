de6383fa44ff54beaf166e90f263b378128678f1
PavelK <laeyenix@gmail.com>
2023-10-21 23:50:02 +0300

    v 0.9 . Добавил работу с бд, ткперь можно создавать ссылки транслитом и переходить по ссылкам.

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -4,6 +4,8 @@ using Microsoft.AspNetCore.Authentication;
 using Microsoft.AspNetCore.Mvc;
 using System.Diagnostics;
 using System.Security.Claims;
+using System.Text;
+using UnidecodeSharpCore;
 
 namespace Hackathon.Controllers
 {
@@ -19,10 +21,19 @@ namespace Hackathon.Controllers
 			_logger = logger;
 		}
 
-		public IActionResult Index()
+		public IActionResult Index(Articles? article)
 		{
-            Articles article = new Articles();
-			return View(article);
+
+			if (article == null)
+			{
+				Articles article1 = new Articles();
+                return View(article1);
+            }
+			else {
+                return View(article);
+            }
+         
+			
 		}
 
 		public IActionResult Privacy()
@@ -33,7 +44,7 @@ namespace Hackathon.Controllers
         [HttpPost("/publish")]
         public IActionResult Publish(string? author, string? title, string? subtitle, string? text)
         {
-			if (string.IsNullOrEmpty(author) || string.IsNullOrEmpty(title) || string.IsNullOrEmpty(subtitle) || string.IsNullOrEmpty(text)) {
+			if (string.IsNullOrEmpty(author) || string.IsNullOrEmpty(title) ||  string.IsNullOrEmpty(text)) {
 				//todo всплывающее окно
 				return NotFound();
 			}
@@ -47,16 +58,64 @@ namespace Hackathon.Controllers
 			Console.WriteLine(subtitle);
 			Console.WriteLine(text);
 			var newArticle = new Articles();
-            newArticle.Id = articles.Max(i=>i.Id+1);
+           // newArticle.Id = articles.Max(i=>i.Id+1);
             newArticle.Author = author;
 			newArticle.Title = title;
 			newArticle.Subtitle = subtitle;
 			newArticle.Article = text;
 			newArticle.publication_time = DateTime.Now;
-          //  db.Articles.Add(newArticle);
-           // db.SaveChanges();
+			newArticle.Link = GenerateLink(title, DateTime.Now);
+            db.Articles.Add(newArticle);
+            db.SaveChanges();
+            
             Console.WriteLine("Успешный успех");
             return Ok();
+        }
+        public ActionResult Show(string articleUrl)
+        {
+
+            Console.WriteLine($"articleUrl: {articleUrl}");
+
+            var articles = db.Articles;
+            Articles? neededArticle = articles.FirstOrDefault(a=> a.Link == articleUrl);
+			if (neededArticle == null) {
+				//todo
+				return NotFound();
+			}
+
+            return View("Index", neededArticle);
+        }
+
+        public string GenerateLink(string title, DateTime date)
+        {
+            // Удаление пробелов и транслитерация символов
+            string sanitizedTitle = TransliterateAndSanitize(title);
+
+            // Форматирование даты в виде "месяц-день"
+            string formattedDate = date.ToString("MM-dd");
+
+            // Склейка частей URL
+            string url = $"{sanitizedTitle}-{formattedDate}";
+
+            return url;
+        }
+
+        private string TransliterateAndSanitize(string input)
+        {
+
+            StringBuilder result = new StringBuilder();
+            foreach (char c in input.ToLower().Unidecode())
+            {
+                if (char.IsLetterOrDigit(c))
+                {
+                    result.Append(c);
+                } else if (c == ' ')
+                {
+                    result.Append('-');
+                }
+            }
+
+            return result.ToString();
         }
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
