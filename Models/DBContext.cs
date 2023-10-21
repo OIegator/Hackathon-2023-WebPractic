37de66b6707e254ec1c570aa0c85b4b970fc2824
OIegator <olegshev506@gmail.com>
2023-10-21 21:03:40 +0300

    Initial commit

---
+++ Models/DBContext.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Hackathon.Models;
+
+namespace Hackathon.Data
+{
+    public class DBContext : DbContext
+    {
+        public DBContext(DbContextOptions<DBContext> options) : base(options) { }
+
+        public DbSet<Articles> Articles { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            base.OnConfiguring(optionsBuilder);
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Articles>().ToTable("Articles");
+        }
+    }
+}
