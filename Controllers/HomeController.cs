19e87b2dc9664b82bda1f928ccf0a2579527e28d
Pavel Kuzub <laeyenix@gmail.com>
2023-10-22 13:11:45 +0300

    рефакторинг

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -15,20 +15,19 @@ namespace Hackathon.Controllers
 	public class HomeController : Controller
 	{
 		private readonly ILogger<HomeController> _logger;
-		private readonly IWebHostEnvironment webHostEnvironment;
 		private readonly DBContext db;
+		private readonly ArticleUseCase articleUseCase;
 
-
-        public HomeController(ILogger<HomeController> logger, DBContext _db, IWebHostEnvironment _webHostEnvironment)
+        public HomeController(ILogger<HomeController> logger, DBContext _db)
 		{
 			db = _db;
 			_logger = logger;
-			webHostEnvironment = _webHostEnvironment;
+			articleUseCase= new ArticleUseCase();
 		}
 
+		// основной экран, для уменьшение количества кода и простоты судейства было решено не разбивать на 2 отдельных экрана почти идентичный по функционалу код
 		public IActionResult Index(Articles? article)
 		{
-            
 			if (article == null)
 			{
 				Articles article1 = new Articles();
@@ -41,11 +40,7 @@ namespace Hackathon.Controllers
 			
 		}
 
-		public IActionResult Privacy()
-		{
-			return View();
-		}
-
+		// публикация статьи из заполненных полей
         [HttpPost("/publish")]
         public IActionResult Publish(string? author, string? title, string? subtitle, string? text, string? style)
         {
@@ -54,73 +49,41 @@ namespace Hackathon.Controllers
 			}
 		
 			var newArticle = new Articles();
-           // newArticle.Id = articles.Max(i=>i.Id+1);
             newArticle.Author = author;
 			newArticle.Title = title;
 			newArticle.Subtitle = subtitle;
 			newArticle.Article = text;
 			newArticle.publication_time = DateTime.Now;
 			newArticle.Style = style;
-			newArticle.Link = GenerateLink(title, DateTime.Now);
+			newArticle.Link = articleUseCase.GenerateLink(title, DateTime.Now);
             db.Articles.Add(newArticle);
             db.SaveChanges();
 
-            return CreateQRCode(newArticle.Link);
+            return CreateArticleUrl(newArticle.Link);
         }
+		// показ найденной статьи
         public ActionResult Show(string articleUrl)
         {
             var articles = db.Articles;
             Articles? neededArticle = articles.FirstOrDefault(a=> a.Link == articleUrl);
 			if (neededArticle == null) {
-				//todo
 				return NotFound();
 			}
 
             return View("Index", neededArticle);
         }
 
-        public string GenerateLink(string title, DateTime date)
-        {
-            // Удаление пробелов и транслитерация символов
-            string sanitizedTitle = TransliterateAndSanitize(title);
-
-            // Форматирование даты в виде "месяц-день"
-            string formattedDate = date.ToString("MM-dd");
-
-            // Склейка частей URL
-            string url = $"{sanitizedTitle}-{formattedDate}";
-
-            return url;
-        }
-
-		// транслит строки, изменение регистра, замена пробелов
-        private string TransliterateAndSanitize(string input)
-        {
-            StringBuilder result = new StringBuilder();
-            foreach (char c in input.ToLower().Unidecode())
-            {
-                if (char.IsLetterOrDigit(c))
-                {
-                    result.Append(c);
-                } else if (c == ' ')
-                {
-                    result.Append('-');
-                }
-            }
-            return result.ToString();
-        }
-
-
-		public IActionResult CreateQRCode(string link)
+		// Создание ссылки новой статьи
+		public IActionResult CreateArticleUrl(string link)
 		{
 			string articleUrl = HttpContext.Request.Scheme + "://" + HttpContext.Request.Host.Value + "/article=" + link;
-			return Json(new {url = articleUrl });
+			return Ok(new {url = articleUrl });
 		}
 
+		// СОЗДАНИЕ ИЗОБРАЖЕНИЯ QR
 		public ActionResult GenerateQRCode(string text)
 		{
 			GeneratedBarcode barcode = QRCodeWriter.CreateQrCode(text, 200);
-
 			barcode.SetMargins(10);
 			Bitmap bitmap = barcode.ToBitmap();
 			barcode.ChangeBarCodeColor(IronSoftware.Drawing.Color.Black);
@@ -131,7 +94,6 @@ namespace Hackathon.Controllers
 			}
 		}
 
-			//todo решить что делать
 			[ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
 		public IActionResult Error()
 		{
